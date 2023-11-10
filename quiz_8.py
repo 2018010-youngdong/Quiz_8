67cec64e3c73de33a521f9e04e7c1b10aa371cc6
2018010-youngdong <144421629+2018010-youngdong@users.noreply.github.com>
2023-11-10 16:51:35 +0900

    quiz_8
    랜덤의 중복되지않는 6가지 숫자를 출력하는 함수입니다.

--- quiz_8.py
+++ quiz_8.py
@@ -1,9 +1,11 @@
 import random
-result = []
-while len(result) < 6:
-    number = random.randint(1, 45)
-    if number not in result:
-        result.append(number)
-
-result.sort()
-print('생성된 숫자는 ' + str(result) + '입니다.')
+def random_number():
+    result = []
+    while len(result) < 6:
+        number = random.randint(1, 45)
+        if number not in result:
+            result.append(number)
+    result.sort()
+    return result
+x=random_number()
+print('생성된 숫자는 ' + str(x) + '입니다.')
